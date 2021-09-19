7375951fb0bbc916910e704d4b75889dc18ed362
e-number <eggnom@yandex.ru>
2021-09-19 19:04:20 +0300

    1909

---
+++ Solution/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public static String humanReadableTime(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
+    }
+
+    public static int digitalRoot(int n) {
+        return (1 + (n - 1) % 9);
+    }
+}

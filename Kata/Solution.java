5d4cf018aecf25d35dc3080d326d5b35729363bc
e-number <eggnom@yandex.ru>
2021-09-20 20:58:57 +0300

    2009

--- Kata/Solution.java
+++ Kata/Solution.java
@@ -1,11 +1,30 @@
+import java.util.ArrayList;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        humanReadableTime(75533);
+        digitalRoot(16);
+        between(1, 4);
+    }
     public static String humanReadableTime(int seconds) {
-        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
+        String humanReadableTime = String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
+        System.out.println(humanReadableTime);
+        return humanReadableTime;
     }
 
     public static int digitalRoot(int n) {
-        return (1 + (n - 1) % 9);
+        int digRoot = (1 + (n - 1) % 9);
+        System.out.println(digRoot);
+        return digRoot;
+    }
+
+
+    public static int[] between(int a, int b) {
+        int nums[] = new int[0];
+        for (int i = a; i <= b; i++) {
+            System.out.println(i);
+        }
+        return nums;
     }
 }
